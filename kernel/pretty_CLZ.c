13c7e9b9d54880e5b73329d9c9e3bdd361c381c3
Yahia Farghaly <yahiafarghaly@gmail.com>
2020-08-01 22:59:09 +0200

    Added O(1) C implementation of clz instruction.
    This is done using a table of fixed known values of leading zeros of values ranging from 0 to 255
    plus some bitwise operations.
    
    It consumes more CPU cycles than if a clz assembly instruction is provided but it's better than using
    the known loop methods to count the number of leading zero bits in a given number.
    
    Signed-off-by: Yahia Farghaly <yahiafarghaly@gmail.com>

--- kernel/pretty_CLZ.c
+++ kernel/pretty_CLZ.c
@@ -25,7 +25,8 @@ SOFTWARE.
 /*
  * Author   : Yahia Farghaly Ashour
  *
- * Purpose  : Contains the implementation of CPU_CountLeadZeros() instruction in C. If no assembly instruction is supported.
+ * Purpose  : Contains the implementation of CPU_CountLeadZeros()in C
+ *            which can be used if no assembly instruction is supported by the target processor/Compiler.
  *
  * Language:  C
  */
@@ -46,35 +47,117 @@ SOFTWARE.
 
 #if(CPU_CONFIG_COUNT_LEAD_ZEROS_ASM_PRESENT == 0U)
 
+/*
+ * Fixed values of known leading zeros of numbers from 0x00 to 0xFF.
+ * This will be used for fast calculating the leading zeros for numbers larger than 0xFF ( more than 1 byte.)           */
+static const CPU_t08U CPU_CntLeadZerosTbl[256] = {                                  /* Data values :                    */
+/*   0    1    2    3    4    5    6    7    8    9    A    B    C    D    E    F   */
+    8u,  7u,  6u,  6u,  5u,  5u,  5u,  5u,  4u,  4u,  4u,  4u,  4u,  4u,  4u,  4u,  /*   0x00 to 0x0F                   */
+    3u,  3u,  3u,  3u,  3u,  3u,  3u,  3u,  3u,  3u,  3u,  3u,  3u,  3u,  3u,  3u,  /*   0x10 to 0x1F                   */
+    2u,  2u,  2u,  2u,  2u,  2u,  2u,  2u,  2u,  2u,  2u,  2u,  2u,  2u,  2u,  2u,  /*   0x20 to 0x2F                   */
+    2u,  2u,  2u,  2u,  2u,  2u,  2u,  2u,  2u,  2u,  2u,  2u,  2u,  2u,  2u,  2u,  /*   0x30 to 0x3F                   */
+    1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  /*   0x40 to 0x4F                   */
+    1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  /*   0x50 to 0x5F                   */
+    1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  /*   0x60 to 0x6F                   */
+    1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  1u,  /*   0x70 to 0x7F                   */
+    0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  /*   0x80 to 0x8F                   */
+    0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  /*   0x90 to 0x9F                   */
+    0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  /*   0xA0 to 0xAF                   */
+    0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  /*   0xB0 to 0xBF                   */
+    0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  /*   0xC0 to 0xCF                   */
+    0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  /*   0xD0 to 0xDF                   */
+    0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  /*   0xE0 to 0xEF                   */
+    0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u,  0u   /*   0xF0 to 0xFF                   */
+};
+
+#endif
+
+#if(CPU_CONFIG_COUNT_LEAD_ZEROS_ASM_PRESENT == 0U)
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 #if (CPU_CONFIG_DATA_SIZE_BITS == CPU_WORD_SIZE_08)
-CPU_tWORD CntLeadZeros08 (CPU_t08U val)
+CPU_tWORD inline CntLeadZeros08 (CPU_t08U val)
 {
-    return (0);
+    return (CPU_tWORD)(CPU_CntLeadZerosTbl[(CPU_tWORD)val]);
 }
 #endif
 
 #if (CPU_CONFIG_DATA_SIZE_BITS == CPU_WORD_SIZE_16)
-CPU_tWORD CntLeadZeros16 (CPU_t16U val)
+CPU_tWORD inline CntLeadZeros16 (CPU_t16U val)
 {
-    return (0);
+    if(val > 0x00FFU)
+    {
+        return (CPU_tWORD)(CPU_CntLeadZerosTbl[(CPU_tWORD)(val >> 8U)]);
+    }
+    else
+    {
+        return (CPU_tWORD)(CPU_CntLeadZerosTbl[(CPU_tWORD)val] + 8U);   /* 8 bits of most byte + #number of bits in the  least byte. */
+    }
 }
 #endif
 
 #if (CPU_CONFIG_DATA_SIZE_BITS == CPU_WORD_SIZE_32)
-CPU_tWORD CntLeadZeros32 (CPU_t32U val)
+CPU_tWORD inline CntLeadZeros32 (CPU_t32U val)
 {
-    return (0);
+    if(val > 0x0000FFFFU)
+    {
+        if(val > 0x00FFFFFFU)
+        {
+            return (CPU_tWORD)(CPU_CntLeadZerosTbl[(CPU_tWORD)(val >> 24U)] + 0U);
+        }
+        else
+        {
+            return (CPU_tWORD)(CPU_CntLeadZerosTbl[(CPU_tWORD)(val >> 16U)] + 8U);
+        }
+    }
+    else
+    {
+        if(val > 0x00FFU)
+        {
+            return (CPU_tWORD)(CPU_CntLeadZerosTbl[(CPU_tWORD)(val >> 8U)] + 16U);
+        }
+        else
+        {
+            return (CPU_tWORD)(CPU_CntLeadZerosTbl[(CPU_tWORD)val] + 24U);
+        }
+    }
 }
 #endif
 
 #if (CPU_CONFIG_DATA_SIZE_BITS == CPU_WORD_SIZE_64)
-CPU_tWORD CntLeadZeros64 (CPU_t64U val)
+CPU_tWORD inline CntLeadZeros64 (CPU_t64U val)
 {
-    return (0);
+    if (val > 0x00000000FFFFFFFFU) {
+        if(val > 0x0000FFFFFFFFFFFFU)
+        {
+            if(val > 0x00FFFFFFFFFFFFFFU)
+            {
+                return (CPU_tWORD)(CPU_CntLeadZerosTbl[(CPU_tWORD)(val >> 56U)] + 0U);
+            }
+            else
+            {
+                return (CPU_tWORD)(CPU_CntLeadZerosTbl[(CPU_tWORD)(val >> 48U)] + 8U);
+            }
+        }
+        else
+        {
+            if (val > 0x000000FFFFFFFFFFU)
+            {
+                return (CPU_tWORD)(CPU_CntLeadZerosTbl[(CPU_tWORD)(val >> 40U)] + 16U);
+            }
+            else
+            {
+                return (CPU_tWORD)(CPU_CntLeadZerosTbl[(CPU_tWORD)(val >> 32U)] + 24U);
+            }
+        }
+    }
+    else
+    {
+        return (CPU_tWORD)(CntLeadZeros32((CPU_t32U)val) + 32U);
+    }
 }
 #endif
 
@@ -97,7 +180,6 @@ CPU_tWORD CPU_CountLeadZeros(CPU_tWORD val)
 #else                                                         
         #error "Undefined CPU_tWord type."
 #endif
-
 
     return (number_of_lead_zeros);
 }
